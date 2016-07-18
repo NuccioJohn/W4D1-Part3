ebcf5bee0d425f2cdd20cdb06ec6bbaeb686d49e
NuccioJohn <nuccio.john@gmail.com>
2016-07-18 12:16:04 -0400

    gergrgreg

--- app/src/main/java/com/example/simpleloader/MainActivity.java
+++ app/src/main/java/com/example/simpleloader/MainActivity.java
@@ -1,9 +1,13 @@
 package com.example.simpleloader;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.LoaderManager;
+import android.support.v4.content.ContextCompat;
 import android.support.v4.content.CursorLoader;
 import android.support.v4.content.Loader;
 import android.support.v7.app.AppCompatActivity;
@@ -17,12 +21,38 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
 
     private static final int LIST_ID = 10;
 
+    private static final int CODE_READ_SMS = 20;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        getSupportLoaderManager().initLoader(LIST_ID, null, this);
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_SMS) != PackageManager.PERMISSION_GRANTED) {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.READ_SMS)) {
+                Log.d(TAG, "onCreate: " + "Show explanation");
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_SMS}, CODE_READ_SMS);
+            } else {
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_SMS}, CODE_READ_SMS);
+            }
+        } else {
+            Log.d(TAG, "onCreate: " + "Permission already granted!");
+            getSupportLoaderManager().initLoader(LIST_ID, null, this);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case CODE_READ_SMS: {
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    Log.d(TAG, "onRequestPermissionsResult: Good to go!");
+                    getSupportLoaderManager().initLoader(LIST_ID, null, this);
+                } else {
+                    Log.d(TAG, "onRequestPermissionsResult: Bad user");
+                }
+            }
+        }
     }
 
     @Override
