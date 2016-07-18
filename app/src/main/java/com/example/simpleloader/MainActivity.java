793969e019ebc3b59e259fb24eb1471bcad9b325
NuccioJohn <nuccio.john@gmail.com>
2016-07-18 12:44:00 -0400

    new Branch

--- app/src/main/java/com/example/simpleloader/MainActivity.java
+++ app/src/main/java/com/example/simpleloader/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.simpleloader;
 
+
 import android.Manifest;
 import android.content.pm.PackageManager;
 import android.database.Cursor;
@@ -13,7 +14,7 @@ import android.support.v4.content.Loader;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 
-public class MainActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<Cursor> {
+public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "MainActivityTAG_";
 
@@ -37,7 +38,7 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
             }
         } else {
             Log.d(TAG, "onCreate: " + "Permission already granted!");
-            getSupportLoaderManager().initLoader(LIST_ID, null, this);
+            doMagic();
         }
     }
 
@@ -47,7 +48,7 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
             case CODE_READ_SMS: {
                 if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
                     Log.d(TAG, "onRequestPermissionsResult: Good to go!");
-                    getSupportLoaderManager().initLoader(LIST_ID, null, this);
+                    doMagic();
                 } else {
                     Log.d(TAG, "onRequestPermissionsResult: Bad user");
                 }
@@ -55,30 +56,23 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
         }
     }
 
-    @Override
-    public Loader<Cursor> onCreateLoader(int id, Bundle args) {
-        return new CursorLoader(getApplicationContext(),
-                Uri.parse(SMS_CONTENT_URI),
-                null,
-                null,
-                null,
-                null);
-    }
+    public void doMagic() {
+        Uri uri = Uri.parse(SMS_CONTENT_URI);
 
-    @Override
-    public void onLoadFinished(Loader<Cursor> loader, Cursor cursor) {
+        Cursor cursor = getContentResolver().query(uri, null, null, null, null);
+
         if (cursor != null && cursor.getCount() > 0) {
             cursor.moveToFirst();
-            do {
-                final String messageBody = cursor.getString(cursor.getColumnIndex("body"));
-                final String messageAddress = cursor.getString(cursor.getColumnIndex("address"));
-                Log.d(TAG, "onLoadFinished: " + messageAddress + " " + messageBody);
-            } while (cursor.moveToNext());
-        }
-    }
 
-    @Override
-    public void onLoaderReset(Loader<Cursor> loader) {
-        Log.d(TAG, "onLoaderReset: ");
+            do {
+                Log.d(TAG, "doMagic: " +
+                        cursor.getString(cursor.getColumnIndex("body")));
+                Log.d(TAG, "doMagic: " +
+                        cursor.getString(cursor.getColumnIndex("address")));
+
+            } while (cursor.moveToNext());
+
+            cursor.close();
+        }
     }
 }
